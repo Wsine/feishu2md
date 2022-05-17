a138744cec4edccd8d45b25b81fcb9dadfbda73e
Wsine <weizhy3@hotmail.com>
2022-05-17 11:39:23 +0800

    initial workable version

--- go.mod
+++ go.mod
@@ -3,16 +3,19 @@ module example.com/feishu2md
 go 1.17
 
 require (
+	github.com/88250/lute v1.7.3
 	github.com/chyroc/lark v0.0.94
 	github.com/chyroc/lark_docs_md v0.1.0
-	github.com/joho/godotenv v1.4.0
+	github.com/urfave/cli/v2 v2.6.0
 )
 
 require (
+	github.com/alecthomas/chroma v0.9.2 // indirect
 	github.com/chyroc/go-ptr v1.6.0 // indirect
-	github.com/mattn/go-runewidth v0.0.13 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/russross/blackfriday v1.6.0 // indirect
-	github.com/shurcooL/go v0.0.0-20200502201357-93f07166e636 // indirect
-	github.com/shurcooL/markdownfmt v0.0.0-20210117162146-75134924a9fd // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/danwakefield/fnmatch v0.0.0-20160403171240-cbb64ac3d964 // indirect
+	github.com/dlclark/regexp2 v1.4.0 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20210619142842-05447a1fa367 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-  "context"
-  "fmt"
-  "os"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 
-  "github.com/chyroc/lark"
-  "github.com/chyroc/lark/larkext"
-  "github.com/chyroc/lark_docs_md"
-  "github.com/joho/godotenv"
+	"github.com/88250/lute"
+	"github.com/chyroc/lark"
+	"github.com/chyroc/lark/larkext"
+	"github.com/chyroc/lark_docs_md"
+	"github.com/urfave/cli/v2"
 )
+
+var configName string = "feishu.json"
 
 func checkErr(e error) {
   if e != nil {
@@ -17,31 +24,86 @@ func checkErr(e error) {
   }
 }
 
-func main() {
-  godotenv.Load()
-  appId := os.Getenv("FEISHU_APP_ID")
-  appSecret := os.Getenv("FEISHU_APP_SECRET")
+func generateConfig() error {
+  if _, err := os.Stat(configName); errors.Is(err, os.ErrNotExist) {
+    config := Config{
+      Feishu: Feishu{AppId: "", AppSecret: ""},
+      Output: Output{ImageDir: "static"},
+    }
+    file, err := json.MarshalIndent(config, "", " ")
+    checkErr(err)
+    err = ioutil.WriteFile(configName, file, 0644)
+    checkErr(err)
+  }
+  return nil
+}
+
+func handleUrl(url string) error {
+  configFile, err := os.Open(configName)
+  checkErr(err)
+  defer configFile.Close()
+
+  var config Config
+  byteValue, err := ioutil.ReadAll(configFile)
+  checkErr(err)
+  json.Unmarshal(byteValue, &config)
+  fmt.Printf("%+v\n", config)
+
   client := lark.New(
-    lark.WithAppCredential(appId, appSecret),
+    lark.WithAppCredential(config.Feishu.AppId, config.Feishu.AppSecret),
   )
 
-  docToken := os.Getenv("FEISHU_DOC_TOKEN")
+  docToken := url[strings.LastIndex(url, "/") + 1 : ]
   doc, err := larkext.NewDoc(client, docToken).Content(context.Background())
   checkErr(err)
 
   result := lark_docs_md.DocMarkdown(context.Background(), doc, &lark_docs_md.FormatOpt{
     LarkClient: client,
-    StaticDir: "static",
-    FilePrefix: "static",
+    StaticDir: config.Output.ImageDir,
+    FilePrefix: config.Output.ImageDir,
   })
-  fmt.Println(result)
+  engine := lute.New(func (l *lute.Lute) {
+    l.RenderOptions.AutoSpace = true
+  })
+  result = engine.FormatStr("md", result)
 
-  f, err := os.Create("test.md")
+  mdName := fmt.Sprintf("%s.md", docToken)
+  mdFile, err := os.Create(mdName)
   checkErr(err)
-  defer f.Close()
-  nBytes, err := f.WriteString(result)
+  defer mdFile.Close()
+  nBytes, err := mdFile.WriteString(result)
   checkErr(err)
   fmt.Printf("Wrote %d bytes\n", nBytes)
-  f.Sync()
+  mdFile.Sync()
+  return nil
+}
+
+func main() {
+  app := &cli.App{
+    Name: "feishu2md",
+    Usage: "download feishu doc as markdown file",
+    Flags: []cli.Flag{
+      &cli.BoolFlag{
+        Name: "config",
+        Usage: "generate config file",
+      },
+    },
+    Action: func(ctx *cli.Context) error {
+      if ctx.Bool("config") {
+        err := generateConfig()
+        checkErr(err)
+      } else if ctx.NArg() == 0 {
+        cli.ShowAppHelp(ctx)
+      } else {
+        url := ctx.Args().Get(0)
+        err := handleUrl(url)
+        checkErr(err)
+      }
+      return nil
+    },
+  }
+
+  err := app.Run(os.Args)
+  checkErr(err)
 }
 
