2226982fe2a3e43eee4691f7c5fd9af2b658a9c8
Wsine <weizhy3@hotmail.com>
2022-07-19 21:57:04 +0800

    add image download feature

--- core/client_test.go
+++ core/client_test.go
@@ -19,7 +19,7 @@ func getIdAndSecretFromEnv() (string, string) {
 
 func TestNewClient(t *testing.T) {
   appID, appSecret := getIdAndSecretFromEnv()
-  c := core.NewDocClient(appID, appSecret, "feishu.cn")
+  c := core.NewClient(appID, appSecret, "feishu.cn")
   if c == nil {
     t.Errorf("Error creating DocClient")
   }
@@ -27,10 +27,28 @@ func TestNewClient(t *testing.T) {
 
 func TestGetContent(t *testing.T) {
   appID, appSecret := getIdAndSecretFromEnv()
-  c := core.NewDocClient(appID, appSecret, "feishu.cn")
-  content := c.GetContent(context.Background(), "doccnZhhDCLiCP6LJa1nrjDhRSc")
-  fmt.Println(content)
-  if content == "" {
-    t.Errorf("Error: content is empty")
+  c := core.NewClient(appID, appSecret, "feishu.cn")
+  content, err := c.GetDocContent(context.Background(), "doccnZhhDCLiCP6LJa1nrjDhRSc")
+  if err != nil {
+    t.Error(err)
+  }
+  title := content.Title.Elements[0].TextRun.Text
+  fmt.Println(title)
+  if title == "" {
+    t.Errorf("Error: parsed title is empty")
+  }
+}
+
+func TestDownloadImage(t *testing.T) {
+  appID, appSecret := getIdAndSecretFromEnv()
+  c := core.NewClient(appID, appSecret, "feishu.cn")
+  imgToken := "boxcnsXaIKcbwVmvGAwopgu2pre"
+  filename, err := c.DownloadImage(context.Background(), imgToken)
+  if err != nil {
+    t.Error(err)
+  }
+  if filename != "static/" + imgToken + ".png" {
+    fmt.Println(filename)
+    t.Errorf("Error: not expected file extension")
   }
 }
