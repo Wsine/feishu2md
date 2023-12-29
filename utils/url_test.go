0e154857a2dfb7a8e4b252f90b5ea642f6b92b28
Wsine <weizhy3@hotmail.com>
2023-12-29 17:51:33 +0800

    update: command handling

--- utils/url_test.go
+++ utils/url_test.go
@@ -5,40 +5,84 @@ import (
 )
 
 func TestUnescapeURL(t *testing.T) {
-	type args struct {
-		rawURL string
-	}
 	tests := []struct {
-		name string
-		args args
-		want string
+		name   string
+		rawURL string
+		want   string
 	}{
 		{
-			name: "url unescape success",
-			args: args{
-				rawURL: "https%3A%2F%2Fsspai.com%2Fpost%2F58509",
-			},
-			want: "https://sspai.com/post/58509",
+			name:   "url unescape success",
+			rawURL: "https%3A%2F%2Fsspai.com%2Fpost%2F58509",
+			want:   "https://sspai.com/post/58509",
 		},
 		{
-			name: "url unescape failed, keep it",
-			args: args{
-				rawURL: "https$3A$2F$2Fsspai.com$2Fpost$2F58509",
-			},
-			want: "https$3A$2F$2Fsspai.com$2Fpost$2F58509",
+			name:   "url unescape failed, keep it",
+			rawURL: "https$3A$2F$2Fsspai.com$2Fpost$2F58509",
+			want:   "https$3A$2F$2Fsspai.com$2Fpost$2F58509",
 		},
 		{
-			name: "url not need to unescape, keep it",
-			args: args{
-				rawURL: "https://sample.feishu.cn/docs/doccnByZP6puODElAYySJkPIfUb",
-			},
-			want: "https://sample.feishu.cn/docs/doccnByZP6puODElAYySJkPIfUb",
+			name:   "url not need to unescape, keep it",
+			rawURL: "https://sample.feishu.cn/docs/doccnByZP6puODElAYySJkPIfUb",
+			want:   "https://sample.feishu.cn/docs/doccnByZP6puODElAYySJkPIfUb",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := UnescapeURL(tt.args.rawURL); got != tt.want {
-				t.Errorf("unescapeURL() = %v, want %v", got, tt.want)
+			if got := UnescapeURL(tt.rawURL); got != tt.want {
+				t.Errorf("URL = %v\nGot = %v\nExpected = %v", tt.rawURL, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidateDownloadURL(t *testing.T) {
+	type Args struct {
+		url       string
+		allowHost string
+	}
+	tests := []struct {
+		name  string
+		args  Args
+		noErr bool
+	}{
+		{
+			name: "validate feishu url success",
+			args: Args{
+				url:       "https://sample.feishu.cn/docx/doccnByZP6puODElAYySJkPIfUb",
+				allowHost: "",
+			},
+			noErr: true,
+		},
+		{
+			name: "validate larksuite url success",
+			args: Args{
+				url:       "https://sample.larksuite.com/wiki/doccnByZP6puODElAYySJkPIfUb",
+				allowHost: "",
+			},
+			noErr: true,
+		},
+		{
+			name: "validate feishu url success with allow host",
+			args: Args{
+				url:       "https://f.mioffice.cn/docx/doccnByZP6puODElAYySJkPIfUb",
+				allowHost: "f.mioffice.cn",
+			},
+			noErr: true,
+		},
+		{
+			name: "validate arbitrary url failed",
+			args: Args{
+				url:       "https://google.com",
+				allowHost: "",
+			},
+			noErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			url, allowHost := tt.args.url, tt.args.allowHost
+			if _, _, _, got := ValidateDownloadURL(url, allowHost); (got == nil) != tt.noErr {
+				t.Errorf("ValidateDownloadURL(%v, %v)", url, allowHost)
 			}
 		})
 	}
